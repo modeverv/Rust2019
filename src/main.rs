b3b7168d3df254b8c9695e73c6d68531f123921d
nora inu <modeverv@gmail.com>
2019-01-25 18:37:18 +0900

    2019-01-25 18:37:18

--- src/main.rs
+++ src/main.rs
@@ -48,6 +48,28 @@ f64
     } else {
         println!("x は 5 でも 6 でもありません :(");
     }
+
+    let ify = if x == 5 {
+        10
+    } else {
+        15
+    }; // y: i32
+    let ifyy = if x == 5 { 10 } else { 15 }; // y: i32
+
+    // for
+    for x in 0..10 {
+        println!("{}", x);
+    }
+    for (i,j) in (5..10).enumerate() {
+        println!("i = {} and j = {}", i,j);
+    }
+    'outer: for x in 0..10 {
+        'inner: for y in 0..10 {
+            if x % 2 == 0 { continue 'outer; } // x のループを継続
+            if y % 2 == 0 { continue 'inner; } // y のループを継続
+            println!("x: {}, y: {}", x, y);
+        }
+    }
 }
 
 /// # markdown comment!!!
