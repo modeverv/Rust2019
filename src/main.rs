b4f959f6b7754375478969d04c7158d5a8ab7a8e
nora inu <modeverv@gmail.com>
2019-01-25 16:02:58 +0900

    2019-01-25 16:02:58

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
     println!("Hello, world!");
+    // 変数束縛
     let x = 5;
-    let (xx,y) = (1,2);
+    // パターンです
+    let (xx, y) = (1, 2);
+    // 型アノテーション
+    let yy: i32 = 5;
+    let mut yyy = 5;
+    yyy = 10;
+    println!("{}", x);
 }
